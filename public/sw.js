9c347a7e2bd3900b2a13a1fdeef3d61fe6b1760a
Dan Rosart <Torgen@users.noreply.github.com>
2019-05-11 23:54:21 -0700

    Don't reference error in service worker if there's no url

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,9 @@
 self.addEventListener('notificationclick', function(event){
   const n = event.notification;
   n.close();
+  if (!n || !n.data || !n.data.url) {
+    return;
+  }
   clients.matchAll({type: 'window'}).then(function(clientList){
     if (clientList.length > 0) {
       const c = clientList[0];
