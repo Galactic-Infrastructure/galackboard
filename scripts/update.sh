b7e353f1c1d39ae968d723296df84bb219e670e0
Anonymous <anonymous@example.com>
2019-12-07 10:17:08 +0000

    update update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git fetch
+
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
@@ -9,9 +11,10 @@ if [ $LOCAL = $REMOTE ]; then
     echo "Up to date"
 elif [ $LOCAL = $BASE ]; then
     echo "Pulling and reloading..."
-    git pull && bash /home/npinsker/codex-blackboard/scripts/private/update.sh
+    git pull && bash /home/npinsker/codex-blackboard/private/update.sh
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
 else
-    echo "Diverged"
+    echo "Diverged; trying to pull..."
+    git pull && bash /home/npinsker/codex-blackboard/private/update.sh
 fi
